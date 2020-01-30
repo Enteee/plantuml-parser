a502b1d25544a84b1c8ba099ac6b7ce0afb1a436
George Pickering <29524044+geopic@users.noreply.github.com>
2020-01-30 12:36:52 +0000

    Amend types file (add Class, Relationship, others inside namespace)

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,21 +1,67 @@
-type parseOptions = {
-  hiddenPaths?: string[];
-  matchesNode?: boolean;
-  maxPathLength?: number;
-  maxSourceLines?: number;
-  useColor?: boolean;
-  verbose?: boolean;
-}
+declare namespace PlantUMLParser {
+  export class Class {
+    constructor(name: string, title: string, isAbstract: boolean, members: object[]);
+  }
 
-declare class UML {
-  constructor(elements: object[]);
+  export class Component {
+    constructor(name: string, title: string);
+  }
+
+  export class Enum {
+    constructor(name: string, title: string, members: object[]);
+  }
+
+  export class File {
+    constructor(name: string, diagrams: object[]);
+  }
+
+  export class Group {
+    constructor(name: string, title: string, type: string, elements: object[]);
+  }
+
+  export class Interface {
+    constructor(name: string, title: string, members: object[]);
+  }
+
+  export class MemberVariable {
+    constructor(name: string, isStatic: boolean, accessor: string, type: string);
+  }
+
+  export class Method {
+    constructor(name: string, isStatic: boolean, accessor: string, returnType: string, _arguments?: string);
+  }
+
+  export class Note {
+    constructor(text: string, of: string);
+  }
+
+  export class Relationship {
+    constructor(left: string, right: string, leftType: string, rightType: string, leftArrowHead: string, rightArrowHead: string, leftArrowBody: string, rightArrowBody: string, leftCardinality: string, rightCardinality: string, label: string);
+  }
+
+  export class UML {
+    constructor(elements: [PlantUMLParser.Class, PlantUMLParser.Class, PlantUMLParser.Relationship]);
+  }
+
+  export class UseCase {
+    constructor(name: string, title: string);
+  }
+
+  export type parseOptions = {
+    hiddenPaths?: string[];
+    matchesNode?: boolean;
+    maxPathLength?: number;
+    maxSourceLines?: number;
+    useColor?: boolean;
+    verbose?: boolean;
+  }
 }
 
 declare module 'plantuml-parser' {
-  export function parse(data: string, options?: parseOptions): UML;
-  export function parseFile(pattern: string[], options?: parseOptions, cb?: (err: Error, ast: object) => Promise<UML>): UML | Promise<UML>;
+  export function parse(data: string, options?: PlantUMLParser.parseOptions): PlantUMLParser.UML;
+  export function parseFile(pattern: string[], options?: PlantUMLParser.parseOptions, cb?: (err: Error, result: object) => Promise<PlantUMLParser.UML>): PlantUMLParser.UML | Promise<PlantUMLParser.UML>;
   export const formatters: {
-    default: (ast: UML) => string;
-    graph: (ast: UML) => string;
+    default: (result: PlantUMLParser.UML) => string;
+    graph: (result: PlantUMLParser.UML) => string;
   };
 }
