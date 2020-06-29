bebda3f5fb1816a96a9b5b7d3fc21786d5b5847a
Ente <ducksource@duckpond.ch>
2020-06-29 15:01:04 +0200

    format fix & add mocha

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,47 @@
 module.exports = {
-  "env": { "browser": true, "es6": true, "node": true },
-  "extends": ["eslint:recommended"],
-  "globals": { "Atomics": "readonly", "SharedArrayBuffer": "readonly" },
-  "parserOptions": {
-    "ecmaFeatures": { "jsx": true },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+  'env': { 'browser': true, 'es6': true, 'node': true, 'mocha': true },
+  'extends': ['eslint:recommended'],
+  'globals': { 'Atomics': 'readonly', 'SharedArrayBuffer': 'readonly' },
+  'parserOptions': {
+    'ecmaFeatures': { 'jsx': true },
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
   },
-  "plugins": [],
-  "rules": {
-    "indent": ["error", 2, { "SwitchCase": 1 }],
-    "linebreak-style": ["error", "unix"],
-    "quotes": ["error", "single"],
-    "comma-dangle": ["error", "always-multiline"],
-    "semi": ["error", "always"]
+  'plugins': [],
+  'rules': {
+    'indent': ['error', 2, { 'SwitchCase': 1 }],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'semi': ['error', 'always'],
   },
-  "settings": {},
-  "overrides": [
+  'settings': {},
+  'overrides': [
     {
-      "files": ["**/*.ts", "**/*.tsx"],
-      "env": { "browser": true, "es6": true, "node": true },
-      "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended"
+      'files': ['**/*.ts', '**/*.tsx'],
+      'env': { 'browser': true, 'es6': true, 'node': true },
+      'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
       ],
-      "globals": { "Atomics": "readonly", "SharedArrayBuffer": "readonly" },
-      "parser": "@typescript-eslint/parser",
-      "parserOptions": {
-        "ecmaFeatures": { "jsx": true },
-        "ecmaVersion": 2018,
-        "sourceType": "module",
-        "project": "./tsconfig.json"
+      'globals': { 'Atomics': 'readonly', 'SharedArrayBuffer': 'readonly' },
+      'parser': '@typescript-eslint/parser',
+      'parserOptions': {
+        'ecmaFeatures': { 'jsx': true },
+        'ecmaVersion': 2018,
+        'sourceType': 'module',
+        'project': './tsconfig.json',
       },
-      "plugins": ["@typescript-eslint"],
-      "rules": {
-        "indent": ["error", 2, { "SwitchCase": 1 }],
-        "linebreak-style": ["error", "unix"],
-        "quotes": ["error", "single"],
-        "comma-dangle": ["error", "always-multiline"],
-        "@typescript-eslint/no-explicit-any": 0
+      'plugins': ['@typescript-eslint'],
+      'rules': {
+        'indent': ['error', 2, { 'SwitchCase': 1 }],
+        'linebreak-style': ['error', 'unix'],
+        'quotes': ['error', 'single'],
+        'comma-dangle': ['error', 'always-multiline'],
+        '@typescript-eslint/no-explicit-any': 0,
       },
-      "settings": {},
-    }
-  ]
-}
+      'settings': {},
+    },
+  ],
+};
